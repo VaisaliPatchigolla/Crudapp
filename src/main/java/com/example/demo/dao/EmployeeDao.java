e1290d341590affe87ea40bf4492429454ad553a
charanmutyala <charanmutyala4@gmail.com>
2023-12-26 20:41:57 +0530

    First Commit

---
+++ src/main/java/com/example/demo/dao/EmployeeDao.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Employee;
+
+
+
+@Repository
+public interface EmployeeDao extends CrudRepository<Employee,Integer> {
+
+}
